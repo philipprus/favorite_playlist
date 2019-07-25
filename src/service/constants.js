da3de3581c7b566926767b564a008424d02545cc
Philipp Prus <philipp@prcptv.ai>
2019-07-25 11:58:02 +0300

    Get popuar artist

--- src/service/constants.js
+++ src/service/constants.js
@@ -5,7 +5,7 @@ export const MAX_TRACKS = 19;
 export const API_ROOT = "http://api.musixmatch.com/ws/1.1/";
 
 export const ENDPOINTS = {
-    search_track: "track.search?f_has_lyrics=1&s_track_rating=desc",
+    search_track: "track.search?f_has_lyrics=1&s_track_rating=desc&s_artist_rating=desc",
     get_track: "track.get",
     get_cover_album: "album.get",
     get_lyrics_track: "track.lyrics.get"
